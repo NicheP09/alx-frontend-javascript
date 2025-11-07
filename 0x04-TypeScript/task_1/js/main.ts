d4154d364dcc2482874bac2765d287fcce2fc08b
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-07 19:15:52 +0100

    Refactor main.ts to accept any other entry and readonly

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,18 @@
 interface Teacher {
-  firstName: string;
-  lastName: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  contract?: boolean;
+  [key: string]: any;
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
