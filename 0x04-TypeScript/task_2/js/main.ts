05acb79d9424d57b84e1388bab92abffbe890cb3
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-08 08:46:12 +0100

    Add task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,48 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class TeacherClass implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Director | TeacherClass {
+  if (typeof salary === 'number' && salary < 500) {
+    return new TeacherClass();
+  } else {
+    return new Director();
+  }
+}
+
+const employee1 = createEmployee(200);
+
+const employee2 = createEmployee(1000);
+const employee3 = createEmployee('$500');
