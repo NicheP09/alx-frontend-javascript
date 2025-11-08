a738dc5f4b9a41b4d94735ffcc419fe62c15635a
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-08 14:44:46 +0100

    add content to crude.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,5 +1,19 @@
-import { RowID, RowElement } from './interface';
+/// <reference path="./crud.d.ts" />
 
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud.js';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+  ...row,
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
