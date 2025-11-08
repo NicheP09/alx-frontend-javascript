659827feeb4e38dc1f9d7e6ee3d989b70cb7d9fb
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-08 14:56:43 +0100

    refactor syntax

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,19 +1,7 @@
-/// <reference path="./crud.d.ts" />
-
 import { RowID, RowElement } from './interface';
-import * as CRUD from './crud.js';
 
-const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-};
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
 
-const newRowID: RowID = CRUD.insertRow(row);
-
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
-
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+export { insertRow, deleteRow, updateRow };
